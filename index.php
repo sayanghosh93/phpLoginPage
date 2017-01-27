3f3d6f7329d892a7ece5324f5938c982ed71c1aa
Sayan <ghoshsayan93@gmail.com>
2017-01-27 11:18:59 +0530

    Before creating Login and Register page

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <html>
 <head>
   <title>Welcome to your Login Page</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
 </head>
-<body>
-  <h1>Please Login or Register</h1>
-</body>
+ <body>
+   <h1>Please Login or Register</h1>
+   <a href="login.php">Login</a> or
+   <a href="register.php">Register</a>
+ </body>
 </html>
